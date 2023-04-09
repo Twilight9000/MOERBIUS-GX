fce9c1b0dd8e0589f4ccd62cb31c5ae22b05811f
J-Dubbs11 <jmwalker3@mail.bradley.edu>
2023-04-09 00:42:27 -0500

    checklist skeleton script

--- MOERBIUS-GX/Assets/Scripts/ChecklistBehaviour.cs
+++ MOERBIUS-GX/Assets/Scripts/ChecklistBehaviour.cs
@@ -5,15 +5,53 @@ using UnityEngine.UI;
 
 public class ChecklistBehaviour : MonoBehaviour
 {
+    public Text task1;
+    public Text task2;
+    public Text task3;
+
+    public bool task1Done;
+    public bool task2Done;
+    public bool task3Done;
     // Start is called before the first frame update
     void Start()
     {
-        
+        task1.text = ("");
+        task2.text = ("");
+        task3.text = ("");
+
+        task1Done = false;
+        task2Done = false;
+        task3Done = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(task1Done == true)
+        {
+            task1.text = ("Task 1: DONE");
+        }
+        else
+        {
+            task1.text = ("Task 1: NOT DONE");
+        }
+
+        if (task2Done == true)
+        {
+            task2.text = ("Task 2: DONE");
+        }
+        else
+        {
+            task2.text = ("Task 2: NOT DONE");
+        }
+
+        if (task3Done == true)
+        {
+            task3.text = ("Task 3: DONE");
+        }
+        else
+        {
+            task3.text = ("Task 3: NOT DONE");
+        }
     }
 }
