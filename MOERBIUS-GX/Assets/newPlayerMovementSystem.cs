3a0730bfa0a1caa25907dd4b50d6d1d307a4d3ed
Selke Studios <selkestudioss@gmail.com>
2023-05-01 09:29:29 -0500

    The beginning of the new movement system

---
+++ MOERBIUS-GX/Assets/newPlayerMovementSystem.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class newPlayerMovementSystem : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float hInput;
+    public float moveSpeed;
+    public float forwardSpeed;
+
+    public PlayerInput pI;
+    public InputAction hMove;
+
+    private void Awake()
+    {
+        pI = GetComponent<PlayerInput>();
+
+        pI.currentActionMap.Enable();
+
+        hMove = pI.currentActionMap.FindAction("hMove");
+
+        hMove.performed += ctx => hInput = ctx.ReadValue<float>();
+        hMove.canceled += ctx => hInput = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(hInput * moveSpeed, rb.velocity.y, forwardSpeed);
+    }
+}
